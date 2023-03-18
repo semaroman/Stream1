cb0153b83ac8c5edec3371cd10e935daeb192edc
Роман Семашкевич <semaroman@mail.ru>
2023-03-18 11:05:44 +0300

    Исправление

--- src/StreamMain.java
+++ src/StreamMain.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class StreamMain {
-    List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-    Stream<Integer> stream = intList.stream()
-            .filter(x -> x > 0)
-            .filter(x -> x % 2 == 0)
-            .sorted(Comparator.naturalOrder())
-            .forEach(System.out::println);
+    public static void main() {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        Stream<Integer> stream = intList.stream();
+        stream.filter(x -> x > 0)
+                .filter(x -> x % 2 == 0)
+                .sorted(Comparator.naturalOrder())
+                .forEach(System.out::println);
+    }
 }
